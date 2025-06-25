e9857f8081b2ea81a1f1de14f2bad4cf1b3ce993
Seonyoung <joysyl0210@gmail.com>
2025-06-25 18:05:01 -0400

    Turned yes and Nos into 1s and 0s for columns Phone and Url

--- Load.py
+++ Load.py
@@ -3,8 +3,9 @@ import numpy as np
 
 
 df = pd.read_csv("SMS_Dataset/Dataset.csv")
-print(df.head())
 cleaned = df.columns.tolist()
 cleaned.remove('EMAIL')
-print(df[cleaned].head())
+df['PHONE'] = df['PHONE'].map({'yes': '1', 'No': '0'})
+df['URL'] = df['URL'].map({'yes': '1', 'No': '0'})
 df[cleaned].to_csv("SMS_Dataset/Dataset_Cleaned.csv", index = False)
+print(df[cleaned].head())
